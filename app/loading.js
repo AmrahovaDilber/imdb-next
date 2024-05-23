1bdb23344e50d9fac2f36f6b75e464faa5664a4c
AmrahovaDilber <amrahovad911@gmail.com>
2024-05-23 13:52:22 +0400

    added loading

---
+++ app/loading.js
@@ -0,0 +1,8 @@
+export default function loading() {
+    return (
+        <div className="flex justify-center mt-16">
+            <img src='reload.svg' className='h-52'></img>
+        </div>
+    )
+
+}
